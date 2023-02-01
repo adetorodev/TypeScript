f2feef798cbc242eadc4d827e2a03e46350e44d4
Ezekiel Adetoror <adetoroe787@gmail.com>
2023-02-01 23:12:40 +0100

    unior

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
 console.log("Hello There!")
-let singer: String = "Johanna"
+let singer: string = "Johanna"
+
+let bestSong = Math.random() > 0.5 ? "Chian of Fools" : "Respect"
+
+let firstName = "Johanna"
+// firstName.length() // Expression length() is not callable on type number
+
+let mathematician = Math.random() > 0.5 ? undefined : "Mark Godlberg"
+
+let thinker: string | null = null
+if(Math.random() > 0.5){
+    thinker = "Susanne Langer"
+}
+
+let investor: number | string = "Hedy Lamarr"
