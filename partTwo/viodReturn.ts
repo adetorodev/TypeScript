9b74571de1a643a8586e3030e9dd5b3e646eac47
Ezekiel Adetoror <adetoroe787@gmail.com>
2023-02-14 03:22:22 -0500

    function ends

--- partTwo/viodReturn.ts
+++ partTwo/viodReturn.ts
@@ -18,4 +18,15 @@ function returnsVoid() {
 }
 let lazyValue: string | undefined;
 lazyValue = returnsVoid();
-    // Error: Type 'void' is not assignable to type 'string | undefined'.
+// Error: Type 'void' is not assignable to type 'string | undefined'.
+
+
+function createDate(timestamp: number): Date;
+function createDate(month: number, day: number, year: number): Date;
+function createDate(monthOrTimestamp: number, day?: number, year?: number) {
+    return day === undefined || year === undefined
+        ? new Date(monthOrTimestamp)
+        : new Date(year, monthOrTimestamp, day);
+}
+createDate(554356800); // Ok
+createDate(7, 27, 1987); // Ok
