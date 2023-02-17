3d01d1859e9110663f4e63f5f371b16179eee75c
Ezekiel Adetoror <adetoroe787@gmail.com>
2023-02-17 20:47:05 +0100

    readonly

--- partTwo/interfaces/properties.ts
+++ partTwo/interfaces/properties.ts
@@ -12,3 +12,26 @@ const ok: Book = {
 const missing: Book = {
     pages: 80
 };
+
+// readonly
+interface Page {
+    readonly text: string;
+}
+function read(page: Page) {
+    // Ok: reading the text property doesn't attempt to modify it
+    console.log(page.text);
+    page.text += "!";
+    //
+    ~~~~
+    // Error: Cannot assign to 'text'
+    // because it is a read-only property.
+}
+
+const pageIsh = {
+    text: "Hello, world!",
+};
+// Ok: messengerIsh is an inferred object type with text, not a Page
+page.text += "!";
+// Ok: read takes in Page, which happens to
+// be a more specific version of pageIsh's type
+read(messengerIsh);
