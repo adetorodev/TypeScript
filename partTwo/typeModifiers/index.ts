8f37469787121747c7262f253b607f810b51df88
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-04-24 17:05:20 +0100

    unknown type

--- partTwo/typeModifiers/index.ts
+++ partTwo/typeModifiers/index.ts
@@ -1,3 +1,8 @@
+/***********************
+ *  top type
+ * ******************* */ 
+
+
 // any type accept any type of data
 
 let anyValue: any;
@@ -11,3 +16,28 @@ function greetComedian(name: any) {
 greetComedian({ name: "Bea Arthur" })
 // Runtime errror: name.toUpperCase is not a function
 // name is declared as any, TypeScript does not report a type complaint
+
+// unknown
+
+// The key difference with unknown is that TypeScript is much more restrictive about values of type unknown:
+// • TypeScript does not allow directly accessing properties of unknown typed values.
+// • unknown is not assignable to types that are not a top type (any or unknown).
+
+// Attempting to access a property of an unknown typed value, as in the following
+// snippet, will cause TypeScript to report a type error:
+
+function greetComed(name: unknown){
+    console.log(`Announcing ${name.toUpperCase()}`);
+    // error: 'name' is of type 'unknown'
+}
+
+function greetCommedianSafety(name: unknown){
+    if(typeof name === "string"){
+        console.log(`Announcing ${name.toUpperCase()}`);
+    } else {
+        console.log("Well, I'm off.");
+    }
+}
+
+greetCommedianSafety("Betty White")
+greetCommedianSafety({})
