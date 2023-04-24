2719d4ac9a76d7ca4a472d90c93135db6afdaf91
Ezekiel Adetoro <adetoroe787@gmail.com>
2023-04-24 17:18:30 +0100

    type predicate

--- partTwo/typeModifiers/index.ts
+++ partTwo/typeModifiers/index.ts
@@ -1,6 +1,6 @@
 /***********************
  *  top type
- * ******************* */ 
+ * ******************* */
 
 
 // any type accept any type of data
@@ -26,13 +26,13 @@ greetComedian({ name: "Bea Arthur" })
 // Attempting to access a property of an unknown typed value, as in the following
 // snippet, will cause TypeScript to report a type error:
 
-function greetComed(name: unknown){
+function greetComed(name: unknown) {
     console.log(`Announcing ${name.toUpperCase()}`);
     // error: 'name' is of type 'unknown'
 }
 
-function greetCommedianSafety(name: unknown){
-    if(typeof name === "string"){
+function greetCommedianSafety(name: unknown) {
+    if (typeof name === "string") {
         console.log(`Announcing ${name.toUpperCase()}`);
     } else {
         console.log("Well, I'm off.");
@@ -41,3 +41,53 @@ function greetCommedianSafety(name: unknown){
 
 greetCommedianSafety("Betty White")
 greetCommedianSafety({})
+
+// type predicates
+
+// function isNumberOrString(value: unknown) {
+//     return ['number', 'string'].includes(typeof value)
+// }
+
+// function logValueIfExists(value: number | string | null | undefined) {
+//     if (isNumberOrString(value)) {
+//         // type of value: string | number | null | undefined
+//         value.toString();
+//         // error: 'value' is possibly 'null' or 'undefined'
+//     } else {
+//         console.log("Value does not exit:", value);
+//     }
+// }
+
+function isNumberOrString(value: unknown): value is number | string {
+    return ['number', 'string'].includes(typeof value);
+}
+function logValueIfExists(value: number | string | null | undefined) {
+    if (isNumberOrString(value)) {
+        // Type of value: number | string
+        value.toString(); // Ok
+    } else {
+        // Type of value: null | undefined
+        console.log("value does not exist:", value);
+    }
+}
+
+
+interface Comedian {
+    funny: boolean;
+}
+interface StandupComedian extends Comedian {
+    routine: string;
+}
+function isStandupComedian(value: Comedian): value is StandupComedian {
+    return 'routine' in value;
+}
+function workWithComedian(value: Comedian) {
+    if (isStandupComedian(value)) {
+        // Type of value: StandupComedian
+        console.log(value.routine); // Ok
+    }
+    // Type of value: Comedian
+    console.log(value.routine);
+    //
+// Error: Property 'routine' does not exist on type 'Comedian'.
+}
